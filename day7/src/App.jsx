d4f1ad8887503153919f476dc932e31820d1daa5
VenkateshMogili <mogilivenkatesh3@gmail.com>
2024-11-18 16:54:37 +0530

    day7 to 9 added

---
+++ day7/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    {/* TODO: 1.Implement the Portfolio application */}
+      {/* TODO: 2.Push the code to github */}
+      {/* TODO: 3.Deploy into Netlify */}
+    </>
+  )
+}
+
+export default App
